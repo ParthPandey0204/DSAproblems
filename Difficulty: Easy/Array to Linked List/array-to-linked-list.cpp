148cf06b5fd4d1ed5a2a219b5b4394be27a643b8
Parth Pandey <parth.pandey80045@gmail.com>
2025-01-28 03:28:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -49,17 +49,14 @@ public:
 class Solution {
   public:
     Node* constructLL(vector<int>& arr) {
-           Node* head = new Node(arr[0]);
-    if(head==NULL) return nullptr;
-
-       Node* mover =head;
-    for(int i=1;i<arr.size();i++){
-       Node* temp= new Node(arr[i]);
-     mover->next=temp;
-       mover=temp;
-       
-    }
-    return head;
+        Node*head = new Node(arr[0]);
+        Node*move = head;
+        for(int i=1;i<arr.size();i++){
+            Node*temp = new Node(arr[i]);
+            move->next = temp;
+            move = temp;
+        }
+        return head;
     }
 };
 
