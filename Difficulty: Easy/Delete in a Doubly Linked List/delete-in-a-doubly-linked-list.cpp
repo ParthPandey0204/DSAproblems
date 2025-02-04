9b86f1d4e28eee039cdf918d756af79f36eaa705
Parth Pandey <parth.pandey80045@gmail.com>
2025-02-04 06:40:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete in a Doubly Linked List/delete-in-a-doubly-linked-list.cpp
@@ -0,0 +1,170 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// A node of the doubly linked list
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// Function to insert a node at the end
+// of the Doubly Linked List
+Node *push(Node *tail, int new_data) {
+    // Allocate node
+    Node *newNode = new Node(new_data);
+
+    // Since we are adding at the end, next is NULL
+    newNode->next = NULL;
+
+    // Make newNode's previous pointer point to the current tail
+    newNode->prev = tail;
+
+    // If the tail is not null, link it to the new node
+    if (tail != NULL)
+        tail->next = newNode;
+
+    // Return the new node as the new tail
+    return newNode;
+}
+
+bool verifyDLL(Node *head) {
+    int c1 = 0;
+    Node *tmp = head;
+    while (tmp->next) {
+        c1++;
+        tmp = tmp->next;
+    }
+    int c2 = 0;
+    while (tmp->prev) {
+        c2++;
+        tmp = tmp->prev;
+    }
+    return c1 == c2;
+}
+
+// Function to print nodes in a given doubly linked list
+void printList(Node *head) {
+    // If list is empty
+    if (head == NULL)
+        return;
+    if (!verifyDLL(head))
+        return;
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/* Structure of Node
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Function to delete a node at given position.
+    Node* deleteNode(Node* head, int x) {
+             if(head == NULL){
+            return NULL;
+        }
+
+        // If deleting the head node
+        if(x == 1) {
+            Node* temp = head;
+            head = head->next;  // Move head to the next node
+            if(head != NULL) head->prev = NULL;  // Update the previous pointer of the new head
+            delete temp;  // Delete the old head
+            return head;
+        }
+
+        Node* knode = head;
+        int cnt = 1;
+
+        // Traverse to the xth node
+        while(knode != NULL && cnt < x) {
+            knode = knode->next;
+            cnt++;
+        }
+
+        // If the position is out of range (knode == NULL)
+        if(knode == NULL) return head;
+
+        Node* previ = knode->prev;
+        Node* front = knode->next;
+
+        // Update the previous and next pointers of the adjacent nodes
+        if(previ != NULL) previ->next = front;
+        if(front != NULL) front->prev = previ;
+
+        // Delete the node
+        knode->next = nullptr;
+        knode->prev = nullptr;
+        delete knode;
+
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Node *head = new Node(arr[0]);
+        Node *tail = head;
+
+        // Check if the array is empty
+        for (size_t i = 1; i < arr.size(); ++i) {
+            tail = push(tail, arr[i]);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Solution ob;
+        head = ob.deleteNode(head, x);
+
+        printList(head);
+        cout << "\n";
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
